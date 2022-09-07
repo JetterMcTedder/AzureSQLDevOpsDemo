3be11f21737f642bdcda19d6f147bdc9068f2444
Brian Spendolini <btspendo@gmail.com>
2022-09-07 09:07:17 -1000

    v1.0

--- Code/tSQLt/tSQLt.class.sql
+++ Code/tSQLt/tSQLt.class.sql
@@ -13,6 +13,10 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+EXEC sp_configure 'clr strict security', 0;
+RECONFIGURE;
+GO
+
 DECLARE @Msg NVARCHAR(MAX);SELECT @Msg = 'Installed at '+CONVERT(NVARCHAR,GETDATE(),121);RAISERROR(@Msg,0,1);
 GO
 
