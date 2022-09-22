5410fbc6649e4f1dfee785917d51c03c712239cb
Brian Spendolini <btspendo@gmail.com>
2022-09-22 12:05:21 -1000

    v1.0

---
+++ TestsSQLServer/myTests/demoExec.sql
@@ -0,0 +1,4 @@
+EXEC sp_configure 'show advanced option', '1'; RECONFIGURE; EXEC sp_configure 'clr strict security', 0; RECONFIGURE; EXEC sp_configure 'clr enabled', 1; RECONFIGURE;
+
+
+EXEC tSQLt.Run 'testFinancialApp';
