2ecc8d150b845002ca229e7e1702f8f62b99dacd
Brian Spendolini <69256666+JetterMcTedder@users.noreply.github.com>
2023-03-07 10:10:46 -1000

    Update demoExecAzureSQL.sql

--- TestsSQLServer/myTests/demoExecAzureSQL.sql
+++ TestsSQLServer/myTests/demoExecAzureSQL.sql
@@ -1,4 +1,5 @@
 -- EXEC tSQLt.Run 'testFinancialApp';
 BEGIN;
-THROW 51000, 'Github Actions throw test.', 1;
+--THROW 51000, 'Github Actions throw test.', 1;
+RAISERROR (15600, -1, -1, 'mysp_CreateCustomer');
 end;
